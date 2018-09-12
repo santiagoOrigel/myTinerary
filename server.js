40488ff7a361546ba7584f3f33a677b3c96cab82
santiagoOrigel <43065479+santiagoOrigel@users.noreply.github.com>
2018-09-12 17:33:22 +0200

    user Registration
    User Registration (creation)

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -18,9 +24,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("HELLO WORLD"));
+
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
