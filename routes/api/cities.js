171007978742719512d0c975c278cc283e29d483
santiagoOrigel <43065479+santiagoOrigel@users.noreply.github.com>
2018-09-20 17:28:05 +0200

    Creation and Retrieve Cities
    Creation and Retrieve Cities

--- routes/api/cities.js
+++ routes/api/cities.js
@@ -7,19 +7,19 @@ const router = express.Router();
 router.get("/test", (req, res) => res.json({ msg: "Cities Works" }));
 
 // Load City model
-const User = require("../../models/City");
+const City = require("../../models/City");
 
-// @route   GET api/cities/register
+// @route   POST api/cities/register
 // @desc    Register city
 // @access  Public
 router.post("/register", (req, res) => {
-  User.findOne({ name: req.body.name }).then(city => {
-    if (city) {
-      return res.status(400).json({ email: "City already exists" });
-    }
-    User.findOne({ country: req.body.countr }).then(country => {
-      if (country) {
-        return res.status(400).json({ email: "City already exists" });
+  City.findOne({ name: req.body.name }).then(city => {
+    City.findOne({ country: req.body.country }).then(city => {
+      if (city) {
+        return res.status(400).json({
+          name: "City already exists in such Country.",
+          city: city
+        });
       } else {
         const newCity = new City({
           name: req.body.name,
@@ -34,4 +34,15 @@ router.post("/register", (req, res) => {
     });
   });
 });
+
+// @route   GET api/cities/all
+// @desc    Get all cities
+// @access  Public
+router.get("/all", (req, res) => {
+  const cities = [];
+  City.find().then(cities => {
+    res.json(cities);
+  });
+});
+
 module.exports = router;
