9654719cc6a738e1e5a0b5574780da11e7cbe0f4
santiagoOrigel <43065479+santiagoOrigel@users.noreply.github.com>
2018-10-13 18:20:28 +0200

    Search Filter
    Search Filter

--- client/src/components/location/Cities.js
+++ client/src/components/location/Cities.js
@@ -5,20 +5,53 @@ import { connect } from "react-redux";
 import { getCities } from "../../actions/cityActions";
 
 class Cities extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ""
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+  }
+
   componentDidMount() {
     this.props.getCities();
   }
+
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value.substr(0, 20)
+    });
+  }
+
   render() {
+    let filteredCities = [];
     const cities = this.props.cities || [];
+    const search = this.state;
+
+    filteredCities = cities;
+
+    filteredCities.filter(city => {
+      return city.name.toLowerCase().indexOf(search.search.toLowerCase) !== -1;
+    });
 
     return (
       <div className="cities">
+        <form>
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Search for city"
+            name="cityFilter"
+            value={this.state.search}
+            onChange={this.updateSearch}
+          />
+        </form>
         <div className="container">
           <div className="row">
             <div className="col-mn12">
               <h1 className="lead">
-                {cities.map(function(item, i) {
-                  return <li key={i}>{item.name}</li>;
+                {filteredCities.map(city => {
+                  return <li key={city.id}>{city.name}</li>;
                 })}
               </h1>
             </div>
